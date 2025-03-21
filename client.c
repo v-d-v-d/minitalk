49e6073dfaa236d1759533ae313ed4aeebc79e58
Vi <152437517+v-d-v-d@users.noreply.github.com>
2025-03-21 18:57:07 +0100

    fini

--- client.c
+++ client.c
@@ -10,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-//coucou antho est passe par la
-
 #include "libft/libft.h"
 #include <signal.h>
 #include <sys/wait.h>
